72ce6c902bacf1680366f03bebeaa2bf0a48344e
seetharamamilletsandnuts <seetharamamilletsandnuts@gmail.com>
2025-08-24 19:48:08 +0530

    Updated full working version6

--- client/vite.config.js.js
+++ client/vite.config.js.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// client/vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/',   // make sure it's set to root
+});
