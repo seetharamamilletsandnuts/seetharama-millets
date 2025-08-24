bbed56a1f2acf1278b9fb6b5b6dc604f9e89a916
seetharamamilletsandnuts <seetharamamilletsandnuts@gmail.com>
2025-08-24 22:50:57 +0530

    Updated full working version9

---
+++ server/models/productModel.js
@@ -0,0 +1,39 @@
+// server/models/productModel.js
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    category: {
+      type: String,
+      default: "General",
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product;
